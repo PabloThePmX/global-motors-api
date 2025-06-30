75ba6b6415c7eda9bbce9e3070cb1348d5fa6a50
Pablo <pablosarturi1@gmail.com>
2025-06-30 12:45:10 -0300

    retira o underscore no nome do pacote, padronizando com os outros

--- java-services/discovey-service/src/main/java/br/com/globalmotors/DiscoveyServiceApplication.java
+++ java-services/discovey-service/src/main/java/br/com/globalmotors/DiscoveyServiceApplication.java
@@ -1,4 +1,4 @@
-package br.com.global_motors;
+package br.com.globalmotors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
